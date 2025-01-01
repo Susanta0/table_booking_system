3aa7305d1d026ddfe5505e69af51dc89d5e1590f
Susanta0 <susanta721467@gmail.com>
2025-01-02 03:05:34 +0530

    added

--- client/src/app/component/TableBookingSystem.jsx
+++ client/src/app/component/TableBookingSystem.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/Input";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 const TableBookingSystem = () => {
